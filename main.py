660f748f87b1540b0e87b637edc08f02b9a9ea5d
seomj <smj100394@gmail.com>
2023-03-25 16:06:36 +0900

    os_hash_1

--- main.py
+++ main.py
@@ -8,11 +8,27 @@ def search_dockerhub(image_name):
 
 def get_hash(image_fname):
     #jq 설치 필요
-    #official image
-    # command="sudo docker manifest inspect "+ image_fname +" -v | jq -r '.[0].Descriptor.digest'"
-    #etc
-    command="sudo docker manifest inspect "+ image_fname +" -v | jq -r '.Descriptor.digest'"
-    hash_output = subprocess.check_output(command, shell=True, encoding='utf-8')
+    command="sudo docker manifest inspect "+ image_fname +" -v"
+    output = subprocess.check_output(command, shell=True, encoding='utf-8')
+
+    #Ref로 갯수 확인
+    c_ref = output.count('Ref')
+
+    #1개 이상이면 OS 선택 가능하도록 구현
+    if c_ref == 1:
+        command="sudo docker manifest inspect "+ image_fname +" -v | jq -r '.Descriptor.digest'"
+        hash_output = subprocess.check_output(command, shell=True, encoding='utf-8')
+    else:
+        #OS 출력
+        command="sudo docker manifest inspect "+ image_fname +" -v | jq '.[].Descriptor.platform'"
+        output = subprocess.check_output(command, shell=True, encoding='utf-8')
+        print(output)
+        #OS 선택
+        num = int(input('What do you want docker image? '))
+        command="sudo docker manifest inspect "+ image_fname +" -v | jq -r '.[%d].Descriptor.digest'"%num 
+        hash_output = subprocess.check_output(command, shell=True, encoding='utf-8')
+        print(hash_output)
+
     return hash_output
 
 def cve_test(image_fname):
@@ -31,8 +47,8 @@ def show_image_layer(namespace, name, tag, digest):
 # 메인 함수
 def main():
     dir_path="./pj_doc"
-    image_name=str(input("docker hub image: "))
-    search_dockerhub(image_name)   #해당 함수는 search 기능 빼도 이상없음
+    #image_name=str(input("docker hub image: "))
+    #search_dockerhub(image_name)   #해당 함수는 search 기능 빼도 이상없음
     print("================================================================")
     image_fname= input("docker hub image full name(ex.name:tag): ")
     hash_output = get_hash(image_fname)
@@ -40,8 +56,8 @@ def main():
     namespace, name = image_fname.split('/')
     name, tag = name.split(':')
     print("================================================================")
-    cve_test(image_fname)
-    show_image_layer(namespace, name, tag, hash_output)
+    #cve_test(image_fname)
+    #show_image_layer(namespace, name, tag, hash_output)
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,9 +1,22 @@
-import os, sys
+import os, sys, subprocess
 
 dir_path="./pj_doc"
-#image_fname = "centos:latest"
-image_fname = "biocontainers/python-bz2file:v0.98-1-deb_cv1"
-#command="sudo docker manifest inspect "+ image_fname +" -v | jq -r '.[0].Descriptor.digest'"
-command="sudo docker manifest inspect "+ image_fname +" -v | jq -r '.Descriptor.digest'"
+image_fname = "centos:latest"
+#image_fname = "biocontainers/python-bz2file:v0.98-1-deb_cv1"
+
 command="sudo docker manifest inspect "+ image_fname +" -v"
-os.system(command)
+output = subprocess.check_output(command, shell=True, encoding='utf-8')
+#print(output)
+
+c_ref = output.count('Ref')
+
+if c_ref == 1:
+    command="sudo docker manifest inspect "+ image_fname +" -v | jq -r '.Descriptor.digest'"
+else:
+    command="sudo docker manifest inspect "+ image_fname +" -v | jq '.[].Descriptor.platform'"
+    output = subprocess.check_output(command, shell=True, encoding='utf-8')
+    print(output)
+    num = int(input('What do you want docker image? '))
+    command="sudo docker manifest inspect "+ image_fname +" -v | jq -r '.[%d].Descriptor.digest'"%num 
+    output = subprocess.check_output(command, shell=True, encoding='utf-8')
+    print(output)
